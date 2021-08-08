ed17425d23d735edd3f04156920d8bcaed40c42e
Prakhar Verma <prakharverma1022@gmail.com>
2021-08-08 13:04:59 +0530

    Website hosting

---
+++ calculator.js
@@ -0,0 +1,34 @@
+// jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+
+const app = express();
+app.use(bodyParser(urlencoded({extended:true})));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+    const num1 = parseInt(req.body.num1);
+    const num2 = parseInt(req.body.num2);
+    const sum = num1 + num2;
+    res.send(`The sum is ${sum}`);
+})
+
+app.get("/bmiCalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmiCalculator", (req, res) => {
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    const bmiCalc = weight / (height ** 2);
+    res.send("Bmi is: " + bmiCalc);
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
